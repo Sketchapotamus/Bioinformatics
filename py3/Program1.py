c58d20a083f647654f32a00ea0a25793e61db503
Sketchapotamus <tomffxi@gmail.com>
2020-02-01 15:02:01 -0600

    Work on reverse_palindromes

--- py3/Program1.py
+++ py3/Program1.py
@@ -11,4 +11,16 @@ def reverse_compliment(dna):
             compliment += "G"
     return compliment[::-1]
 
+def reverse_palindromes(dna):
+    palindromes = []
+    if len(dna) >= 6:
+        for x in range(len(dna)-5):
+            if dna[x:x+6] == reverse_compliment(dna[x:x+6]):
+                palindromes.append(dna[x:x+6])
+            print(dna[x:x+6])
+
+    return palindromes
+
+
 print(reverse_compliment('AAGTGCG'))
+print(reverse_palindromes('AAGTGCTCG'))
