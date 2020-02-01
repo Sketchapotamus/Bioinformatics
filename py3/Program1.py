07a61ba34b9be18dcccf8c32e7f3a5187bf1095d
Thomas Winters <tomffxi@gmail.com>
2020-02-01 16:24:31 -0600

    Work on main and progress on reverse palindrome

--- py3/Program1.py
+++ py3/Program1.py
@@ -11,16 +11,34 @@ def reverse_compliment(dna):
             compliment += "G"
     return compliment[::-1]
 
+
 def reverse_palindromes(dna):
     palindromes = []
     if len(dna) >= 6:
         for x in range(len(dna)-5):
             if dna[x:x+6] == reverse_compliment(dna[x:x+6]):
-                palindromes.append(dna[x:x+6])
-            print(dna[x:x+6])
+                palindromes.append((dna[x:x+6], x))
 
     return palindromes
 
 
-print(reverse_compliment('AAGTGCG'))
-print(reverse_palindromes('AAGTGCTCG'))
+def main():
+    cont = True
+    print(input())
+    while cont:
+        print("Functions:\n"
+              "* 1 - Reverse Compliment\n"
+              "* 2 - Reverse Palindrome\n"
+              "* 3 - Quit\n"
+              "Please enter request:", end=' ')
+        request = input()
+        if request == "1":
+            print("Please enter the nucleotide sequence:", end=' ')
+            print(reverse_compliment(input()))
+        if request == "3":
+            cont = False
+
+    return
+
+
+main()
