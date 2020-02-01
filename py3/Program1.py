222c3c21b3a70c7f17a2598011c6487a0217bfca
Thomas Winters <tomffxi@gmail.com>
2020-02-01 17:25:02 -0600

    Added codon dictionary

--- py3/Program1.py
+++ py3/Program1.py
@@ -1,3 +1,23 @@
+
+codon = {
+    "UUU": "F",      "CUU": "L",      "AUU": "I",      "GUU": "V",
+    "UUC": "F",      "CUC": "L",      "AUC": "I",      "GUC": "V",
+    "UUA": 'L',      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
+    "UUG": 'L',      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
+    "UCU": 'S',      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
+    "UCC": 'S',      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
+    "UCA": 'S',      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
+    "UCG": 'S',      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
+    "UAU": 'Y',      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
+    "UAC": 'Y',      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
+    "UAA": '*',      'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
+    "UAG": '*',      'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
+    "UGU": 'C',      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
+    "UGC": 'C',      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
+    "UGA": '*',      'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
+    "UGG": 'W',      'CGG': 'R',      'AGG': 'R',      'GGG': 'G'}
+
+
 def reverse_compliment(dna):
     compliment = ""
     for c in dna:
@@ -24,7 +44,6 @@ def reverse_palindromes(dna):
 
 def main():
     cont = True
-    print(input())
     while cont:
         print("Functions:\n"
               "* 1 - Reverse Compliment\n"
@@ -35,6 +54,10 @@ def main():
         if request == "1":
             print("Please enter the nucleotide sequence:", end=' ')
             print(reverse_compliment(input()))
+        if request == "2":
+            print("Please enter the nucleotide sequence:", end=' ')
+            for r in reverse_palindromes(input()):
+                print(f'{r[0]} at position {r[1]+1}')
         if request == "3":
             cont = False
 
