bc5a75474860dad1db4a5a82c4df714f86c1b45f
Sketchapotamus <tomffxi@gmail.com>
2020-02-04 17:50:48 -0600

    Removed test code from sliding_window.py

--- py3/sliding_window.py
+++ py3/sliding_window.py
@@ -23,28 +23,6 @@ def sliding_window(dna1, dna2, window_size, mismatch_limit):
     return graph
 
 
-def graph_test(dna1, dna2):
-    graph = [["_" for x in range(len(dna1)+1)] for y in range(len(dna2)+1)]
-    graph[0][0] = "#"
-    for y in range(len(dna2)):
-        graph[y+1][0] = dna2[y]
-
-    for x in range(len(dna1)):
-        graph[0][x+1] = dna1[x]
-
-    for y in range(len(dna2)):
-        for x in range(len(dna1)):
-            graph[y+1][x+1] = " "
-
-    return graph
-
-# # _ _ _ _ _
-# _ _ _ _ _ _
-# _ _ _ _ _ _
-# _ _ _ _ _ _
-# _ _ _ _ _ _
-
-
 def main():
     graph = sliding_window("GCGAGTGCTTCGTACCC", "CCAAGTCCGATCGGCT", 5, 2)
     for y in range(len(graph)):
